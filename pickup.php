0326115f603e1177d6abe15ac6a2e0805aded2fd
Kano Haruna <osha.ashimari.reenu@gmail.com>
2023-08-09 11:40:25 +0900

    タイトルやコンテンツの表示文字数を制限するfunctionを作成しました。silckにてthumbnailとtitleを同時に同じスライドとして流したいのですが、二つを同じdivタグに格納すると二枚分のスライドが作成されてしまい、空のスライドが流れるので、とりあえずtitleは消去しています。

--- pickup.php
+++ pickup.php
@@ -2,8 +2,9 @@
     'numberposts' => 5, //表示する記事の数
     'post_type' => 'post',
     'orderby' => 'rand', //ランダム表示
-    'tag' => 'pickup' //追加するタグの名前
+    // 'tag' => 'pickup' //追加するタグの名前
 );
+
 $pickupPosts = get_posts($args);
 if ($pickupPosts) : ?>
     <div class="pickup-part">
@@ -14,16 +15,15 @@ if ($pickupPosts) : ?>
                     <?php $post = $pickupPosts[$i];
                     setup_postdata($post) ?>
                     <figure class="pickup-thumbnail">
-                        <a title="<?php the_title_attribute(); ?>" href="<?php the_permalink(); ?>"><?php echo get_the_post_thumbnail($post->ID, 'l-size'); ?></a>
+                        <a title="<?php the_title_attribute(); ?>" href="<?php the_permalink(); ?>"><?php echo get_the_post_thumbnail($post->ID); ?></a>
                     </figure>
-                    <div class="post-info">
-                        <h3 class=pickup-each-title><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                        <p><a href="<?php the_permalink(); ?>">
-                                <?php the_excerpt(); ?></p>
-                    </div>
+
                 </div>
             <?php } ?>
-            <?php wp_reset_postdata(); ?>
+
         </div>
+
+        <?php wp_reset_postdata(); ?>
     </div>
+
 <?php endif; ?>
