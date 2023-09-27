9f953c25fdfd840e91dd7e0a8500ad76f26299e8
Kano Haruna <osha.ashimari.reenu@gmail.com>
2023-09-27 10:35:29 +0900

    404ページの配置を修正しました。

--- index.php
+++ index.php
@@ -72,10 +72,12 @@ get_header();
 		<div class="top-archive-mobile">
 			<div class="top-archive-part">
 				<div class="archive-dropdown-part">
-					<select class="archive-dropdown-select" onChange='document.location.href=this.options[this.selectedIndex].value;'>
+					<label for="monthly-archive">Archive</label>
+					<select class="archive-dropdown-select" id="monthly-archive" onChange='document.location.href=this.options[this.selectedIndex].value;'>
 						<option value="">Archive</option>
 						<?php wp_get_archives('type=monthly&format=option&show_post_count=1'); ?>
 					</select>
+
 				</div>
 			</div>
 		</div>
@@ -122,7 +124,8 @@ get_header();
 		<div class="top-archive-pc">
 			<div class="top-archive-part">
 				<div class="archive-dropdown-part">
-					<select class="archive-dropdown-select" onChange='document.location.href=this.options[this.selectedIndex].value;'>
+					<label for="monthly-archive"></label>
+					<select class="archive-dropdown-select" id="monthly-archive" onChange='document.location.href=this.options[this.selectedIndex].value;'>
 						<option value="">Archive</option>
 						<?php wp_get_archives('type=monthly&format=option&show_post_count=1'); ?>
 					</select>
--- searchform.php
+++ searchform.php
@@ -21,8 +21,11 @@ $twentytwentyone_unique_id = wp_unique_id('search-form-');
 
 $twentytwentyone_aria_label = !empty($args['aria_label']) ? 'aria-label="' . esc_attr($args['aria_label']) . '"' : '';
 ?>
+
 <form role="search" <?php echo $twentytwentyone_aria_label; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Escaped above. 
 					?> method="get" class="search-form" action="<?php echo esc_url(home_url('/')); ?>">
-	<input type="search" id="<?php echo esc_attr($twentytwentyone_unique_id); ?>" class="search-field" value="<?php echo get_search_query(); ?>" name="s" />
-	<input type="submit" class="search-submit" value="<?php echo esc_attr_x('Search', 'submit button', 'twentytwentyone'); ?>" />
+	<label for="<?php echo esc_attr($twentytwentyone_unique_id); ?>">
+		<input type="search" id="<?php echo esc_attr($twentytwentyone_unique_id); ?>" class="search-field" value="<?php echo get_search_query(); ?>" name="s" />
+		<input type="submit" class="search-submit" value="<?php echo esc_attr_x('Search', 'submit button', 'twentytwentyone'); ?>" />
+	</label>
 </form>
